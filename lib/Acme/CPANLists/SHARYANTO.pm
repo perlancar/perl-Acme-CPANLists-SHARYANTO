8f8a0adf462a56148cb0d5d4dd3b8f94d9eb8ab1
perlancar (@pc-home) <perlancar@gmail.com>
2016-02-20 15:59:10 +0700

    Initial commit

---
+++ lib/Acme/CPANLists/SHARYANTO.pm
@@ -0,0 +1,22 @@
+package Acme::CPANLists::SHARYANTO;
+
+# DATE
+# VERSION
+
+our @Author_Lists = (
+);
+
+our @Module_Lists = (
+    {
+        summary => 'Test list',
+        entries => [
+            {module=>'Array::Iter'},
+            {module=>'Array::Iterator'},
+        ],
+    },
+);
+
+1;
+# ABSTRACT: SHARYANTO's CPAN Lists
+
+=head1 DESCRIPTION
